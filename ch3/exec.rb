ff9bad5bb3a701f9d854fd05569acf80a6d47054
tarorin0305 <dayutakatarorin@gmail.com>
2022-04-24 22:57:01 +0900

    implement NFA

--- ch3/exec.rb
+++ ch3/exec.rb
@@ -1,11 +1,11 @@
 require_relative './rule'
 
-rulebook = DFARulebook.new([
-                             FARule.new(1, 'a', 2), FARule.new(1, 'b', 1),
-                             FARule.new(2, 'a', 2), FARule.new(2, 'b', 3),
-                             FARule.new(3, 'a', 3), FARule.new(3, 'b', 3)
-                           ])
-input_string = 'baaaab'
+# rulebook = DFARulebook.new([
+#                              FARule.new(1, 'a', 2), FARule.new(1, 'b', 1),
+#                              FARule.new(2, 'a', 2), FARule.new(2, 'b', 3),
+#                              FARule.new(3, 'a', 3), FARule.new(3, 'b', 3)
+#                            ])
+# input_string = 'baaaab'
 # dfa = DFA.new(1, [3], rulebook); puts "accept?: #{dfa.accepting?}, current_state: #{dfa.current_state}"
 
 # dfa.read_character('b'); puts "accept?: #{dfa.accepting?}, current_state: #{dfa.current_state}"
@@ -14,5 +14,37 @@ input_string = 'baaaab'
 
 # dfa.read_string('baaaa'); puts "accept?: #{dfa.accepting?}, current_state: #{dfa.current_state}"
 
-dfa_dessign = DFADessign.new(1, [3], rulebook)
-puts dfa_dessign.accepts?(input_string)
+# dfa_dessign = DFADessign.new(1, [3], rulebook)
+# puts dfa_dessign.accepts?(input_string)
+
+rulebook = NFARulebook.new([
+                             FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
+                             FARule.new(2, 'a', 3), FARule.new(2, 'b', 3),
+                             FARule.new(3, 'a', 4), FARule.new(3, 'b', 4)
+                           ])
+
+# p rulebook.next_states(Set[1], 'b')
+# p rulebook.next_states(Set[1, 2], 'a')
+# p rulebook.next_states(Set[1, 3], 'b')
+# p NFA.new(Set[1], [4], rulebook).accepting?
+
+# nfa = NFA.new(Set[1], [4], rulebook); p nfa.accepting?
+# nfa.read_character('b'); p nfa.accepting?
+# nfa.read_character('a'); p nfa.accepting?
+# nfa.read_character('b'); p nfa.accepting?
+# nfa = NFA.new(Set[1], [4], rulebook); p nfa.accepting?
+# nfa.read_string('bbbbb'); p nfa.accepting?
+
+# nfa_design = NFADesgin.new(1, [4], rulebook)
+# p nfa_design.accepts?('bab')
+# p nfa_design.accepts?('bbabb')
+
+rulebook = NFARulebook.new([
+                             FARule.new(1, nil, 2), FARule.new(1, nil, 4),
+                             FARule.new(2, 'a', 3),
+                             FARule.new(3, 'a', 2),
+                             FARule.new(4, 'a', 5),
+                             FARule.new(5, 'a', 6),
+                             FARule.new(6, 'a', 4)
+                           ])
+p rulebook.next_states(Set[1], nil)
--- ch3/rule.rb
+++ ch3/rule.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 # 現在のstate, 入力文字, 遷移先のstate の3つの情報で構成されるのが1つの規則
 class FARule < Struct.new(:state, :character, :next_state)
   def applies_to?(state, character)
@@ -46,5 +48,61 @@ class DFADessign < Struct.new(:start_state, :accept_states, :rulebook)
 
   def accepts?(string)
     to_dfa.tap { |dfa| dfa.read_string(string) }.accepting?
+  end
+end
+
+class NFARulebook < Struct.new(:rules)
+  # 現時点でとりうる状態のリストを渡す
+  def next_states(states, character)
+    states.flat_map { |state| follow_rules_for(state, character) }.to_set
+  end
+
+  def follow_rules_for(state, character)
+    rules_for(state, character).map(&:follow)
+  end
+
+  def rules_for(state, character)
+    rules.select { |rule| rule.applies_to?(state, character) } # detectではなくselectになっているのがDFAとの違い
+  end
+
+  def follow_free_moves(states)
+    more_states = next_states(states, nil) # nilを入力に渡すと遷移できる、自由移動後の状態のリストを返す
+
+    # 現在状態のリストを引数としているので、現在状態のリストが自由移動後の状態のリストを包含している場合は、現在の状態のリストを返す
+    if more_states.subset?(states)
+      states
+    else
+      follow_free_moves(states + more_states)
+    end
+  end
+end
+
+class NFA < Struct.new(:current_states, :accept_states, :rulebook)
+  def accepting?
+    (current_states & accept_states).any?
+  end
+
+  def read_character(character)
+    self.current_states = rulebook.next_states(current_states, character)
+  end
+
+  def read_string(string)
+    string.chars.each do |character|
+      read_character(character)
+    end
+  end
+
+  def current_states
+    rulebook.follow_free_moves(super)
+  end
+end
+
+class NFADesgin < Struct.new(:start_state, :accept_states, :rulebook)
+  def accepts?(string)
+    to_nfa.tap { |nfa| nfa.read_string(string) }.accepting?
+  end
+
+  def to_nfa
+    NFA.new(Set[start_state], accept_states, rulebook)
   end
 end
