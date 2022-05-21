bdf5f0963caa5ba79b51250bcae5d8dee52451f4
tarorin0305 <dayutakatarorin@gmail.com>
2022-05-21 22:47:21 +0900

    finish ch4

--- ch4/exec.rb
+++ ch4/exec.rb
@@ -1,4 +1,4 @@
-require_relative './regular'
+require_relative './analyzer'
 # rulebook = NFARulebook.new([
 #                              FARule.new(0, '(', 1), FARule.new(1, ')', 0),
 #                              FARule.new(1, '(', 2), FARule.new(2, ')', 1),
@@ -66,5 +66,64 @@ rulebook = NPDARulebook.new([
                               PDARule.new(2, nil, 3, '$', ['$'])
                             ])
 npda_design = NPDADesign.new(1, '$', [3], rulebook)
-p npda_design.accepts?('abba')
-p npda_design.accepts?('baabaa')
+# p npda_design.accepts?('abba')
+# p npda_design.accepts?('baabaa')
+# p LexicalAnalyzer.new('y = x * 7').analyze
+# p LexicalAnalyzer.new('if (x < 10) { y = true; x = 0 } else { do-nothing }').analyze
+# p LexicalAnalyzer.new('x = falsehood').analyze
+
+start_rule = PDARule.new(1, nil, 2, '$', ['S', '$'])
+symbol_rules = [
+  # <statement> ::= <while> | <assign>
+  PDARule.new(2, nil, 2, 'S', ['W']),
+  PDARule.new(2, nil, 2, 'S', ['A']),
+
+  # <while> ::= 'w' '(' <expression> ')' '{' <statement> '}'
+  PDARule.new(2, nil, 2, 'W', ['w', '(', 'E', ')', '{', 'S', '}']),
+
+  # <assign> ::= 'v' '=' <expression>
+  PDARule.new(2, nil, 2, 'A', ['v', '=', 'E']),
+
+  # <expression> ::= <less-than>
+  PDARule.new(2, nil, 2, 'E', ['L']),
+
+  # <less-than> ::= <multiply> '<' <less-than> | <multiply>
+  PDARule.new(2, nil, 2, 'L', ['M', '<', 'L']),
+  PDARule.new(2, nil, 2, 'L', ['M']),
+
+  # <multiply> ::= <term> '*' <multiply> | <term>
+  PDARule.new(2, nil, 2, 'M', ['T', '*', 'M']),
+  PDARule.new(2, nil, 2, 'M', ['T']),
+
+  # <term> ::= 'n' | 'v'
+  PDARule.new(2, nil, 2, 'T', ['n']),
+  PDARule.new(2, nil, 2, 'T', ['v'])
+]
+
+token_rules = LexicalAnalyzer::GRAMMAR.map do |rule|
+  PDARule.new(2, rule[:token], 2, rule[:token], [])
+end
+# token_rules は下記のようなPDARuleの集合を作っている
+# 2, "i", 2, "i", []
+# 2, "e", 2, "e", []
+# 2, "w", 2, "w", []
+# 2, "d", 2, "d", []
+# 2, "(", 2, "(", []
+# 2, ")", 2, ")", []
+# 2, "{", 2, "{", []
+# 2, "}", 2, "}", []
+# 2, ";", 2, ";", []
+# 2, "=", 2, "=", []
+# 2, "+", 2, "+", []
+# 2, "*", 2, "*", []
+# 2, "<", 2, "<", []
+# 2, "n", 2, "n", []
+# 2, "b", 2, "b", []
+# 2, "v", 2, "v", []
+puts token_rules
+stop_rule = PDARule.new(2, nil, 3, '$', ['$'])
+rulebook = NPDARulebook.new([start_rule, stop_rule] + symbol_rules + token_rules)
+npda_design = NPDADesign.new(1, '$', [3], rulebook)
+token_string = LexicalAnalyzer.new('while (x < 5) { x = x * 3 }').analyze.join
+p token_string
+p npda_design.accepts?(token_string)
